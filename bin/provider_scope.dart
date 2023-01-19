330d4a34773da93d5e89844a450ed30d6981fe89
Tulio Garcia Diniz <tuliao.gd@gmail.com>
2023-01-19 11:00:06 -0300

    :sparkles: feat: ProviderScope

---
+++ bin/provider_scope.dart
@@ -0,0 +1,5 @@
+import 'package:fmagus/src/functions/provider_scope_function.dart';
+
+void main(List<String> args) {
+  providerScope();
+}
---
+++ lib/src/functions/provider_scope_function.dart
@@ -0,0 +1,61 @@
+import 'dart:io';
+
+/// Function to create Route Generator
+void providerScope() {
+  print(_start);
+
+  _createProviderScope();
+
+  print(_end);
+}
+
+//? PRIVATE
+//+ GETTERS
+String get _start => '''
+
+
+
+╔════════════════════════════════════════════════════════════════════════════╗
+║                                🧙 FMAGUS 🧙                                ║
+╠════════════════════════════════════════════════════════════════════════════╣
+║                      Starting Creating ProviderScope...                    ║
+╚════════════════════════════════════════════════════════════════════════════╝
+''';
+
+String get _end => '''
+
+
+
+╔════════════════════════════════════════════════════════════════════════════╗
+║                                🧙 FMAGUS 🧙                                ║
+╠════════════════════════════════════════════════════════════════════════════╣
+║               ProviderScope Creating completed successfully!               ║
+╚════════════════════════════════════════════════════════════════════════════╝
+''';
+
+String get _providerScope => '''
+import 'package:provider/provider.dart';
+
+class ProviderScope extends MultiProvider {
+  final App app;
+  ProviderScope({
+    super.key,
+    required this.app,
+  }) : super(
+          child: app,
+          providers: [
+            YourProvidersHere...
+          ],
+        );
+}
+''';
+
+//+ FUNCTIONS
+void _createProviderScope() {
+  print('\n\nCreating ProviderScope...\n');
+  File('lib/provider_scope_widget.dart').writeAsStringSync(
+    _providerScope,
+    mode: FileMode.writeOnly,
+  );
+  print('Creating Completed.');
+}
