2800319476e0197bce48001bfb111f2ce7fd34d1
Tulio Garcia Diniz <tuliao.gd@gmail.com>
2023-01-19 09:28:16 -0300

    :recycle: refactor: Melhorando GenerateImageAssetFunction

--- lib/src/functions/generate_image_asset_function.dart
+++ lib/src/functions/generate_image_asset_function.dart
@@ -11,12 +11,30 @@ Future<void> generateImageAsset({
   required String path,
   required String assetName,
 }) async {
-  if (!File(path).existsSync()) {
-    print('\nImage not found.');
-    exit(2);
-  }
+  _checkExists(path);
+  final Image image = decodeImage(File(path).readAsBytesSync())!;
 
-  String start = '''
+  print(_start);
+
+  _createDirectories();
+
+  double newWidth;
+
+  newWidth = _generate(image, assetName, 4, null);
+  newWidth = _generate(image, assetName, 3, newWidth);
+  newWidth = _generate(image, assetName, 2, newWidth);
+  newWidth = _generate(image, assetName, 15, newWidth);
+  newWidth = _generate(image, assetName, 1, newWidth);
+  newWidth = _generate(image, assetName, 75, newWidth);
+
+  print(_end);
+
+  exit(0);
+}
+
+//? PRIVATE
+//+ GETTERS
+String get _start => '''
 
 
 
@@ -27,85 +45,7 @@ Future<void> generateImageAsset({
 ╚════════════════════════════════════════════════════════════════════════════╝
 ''';
 
-  print(start);
-
-  print('\nGenerating Image Asset...\n');
-  print('\nGenerating 4.0x image asset...');
-  Image image = decodeImage(File(path).readAsBytesSync())!;
-  Directory('lib/assets/images/4.0x').createSync(recursive: true);
-  Directory('lib/assets/images/3.0x').createSync();
-  Directory('lib/assets/images/2.0x').createSync();
-  Directory('lib/assets/images/1.5x').createSync();
-  Directory('lib/assets/images/1.0x').createSync();
-  Directory('lib/assets/images/0.75x').createSync();
-  File('lib/assets/images/4.0x/$assetName.png').writeAsBytesSync(
-    encodePng(
-      image,
-    ),
-  );
-
-  print('\nGenerating 3.0x image asset...');
-  int width = image.width;
-  double newWidth = (width * 12) / 16;
-  File('lib/assets/images/3.0x/$assetName.png').writeAsBytesSync(
-    encodePng(
-      copyResize(
-        image,
-        width: newWidth.toInt(),
-        interpolation: Interpolation.linear,
-      ),
-    ),
-  );
-
-  print('\nGenerating 2.0x image asset...');
-  newWidth = (newWidth * 8) / 12;
-  File('lib/assets/images/2.0x/$assetName.png').writeAsBytesSync(
-    encodePng(
-      copyResize(
-        image,
-        width: newWidth.toInt(),
-        interpolation: Interpolation.linear,
-      ),
-    ),
-  );
-
-  print('\nGenerating 1.5x image asset...');
-  newWidth = (newWidth * 6) / 8;
-  File('lib/assets/images/1.5x/$assetName.png').writeAsBytesSync(
-    encodePng(
-      copyResize(
-        image,
-        width: newWidth.toInt(),
-        interpolation: Interpolation.linear,
-      ),
-    ),
-  );
-
-  print('\nGenerating 1.0x image asset...');
-  newWidth = (newWidth * 4) / 6;
-  File('lib/assets/images/1.0x/$assetName.png').writeAsBytesSync(
-    encodePng(
-      copyResize(
-        image,
-        width: newWidth.toInt(),
-        interpolation: Interpolation.linear,
-      ),
-    ),
-  );
-
-  print('\nGenerating 0.75x image asset...');
-  newWidth = (newWidth * 3) / 4;
-  File('lib/assets/images/0.75x/$assetName.png').writeAsBytesSync(
-    encodePng(
-      copyResize(
-        image,
-        width: newWidth.toInt(),
-        interpolation: Interpolation.linear,
-      ),
-    ),
-  );
-  print('Image Asset Generation is complete.');
-  String end = '''
+String get _end => '''
 
 
 
@@ -116,7 +56,114 @@ Future<void> generateImageAsset({
 ╚════════════════════════════════════════════════════════════════════════════╝
 ''';
 
-  print(end);
+//+ FUNCTIONS
+void _checkExists(String path) {
+  if (!File(path).existsSync()) {
+    print('\nImage not found.');
+    exit(2);
+  }
+}
 
-  exit(0);
+void _createDirectories() {
+  Directory('lib/assets/images/4.0x').createSync(recursive: true);
+  Directory('lib/assets/images/3.0x').createSync();
+  Directory('lib/assets/images/2.0x').createSync();
+  Directory('lib/assets/images/1.5x').createSync();
+  Directory('lib/assets/images/1.0x').createSync();
+  Directory('lib/assets/images/0.75x').createSync();
+}
+
+double _generate(Image image, String assetName, int x, double? newWidth) {
+  switch (x) {
+    case 4:
+      {
+        print('\nGenerating 4.0x image asset...');
+        File('lib/assets/images/4.0x/$assetName.png').writeAsBytesSync(
+          encodePng(
+            image,
+          ),
+        );
+        return image.width.toDouble();
+      }
+    case 3:
+      {
+        print('\nGenerating 3.0x image asset...');
+        double nu = (newWidth! * 12) / 16;
+        File('lib/assets/images/3.0x/$assetName.png').writeAsBytesSync(
+          encodePng(
+            copyResize(
+              image,
+              width: nu.toInt(),
+              interpolation: Interpolation.linear,
+            ),
+          ),
+        );
+        return nu;
+      }
+    case 2:
+      {
+        print('\nGenerating 2.0x image asset...');
+        double nu = (newWidth! * 8) / 12;
+        File('lib/assets/images/2.0x/$assetName.png').writeAsBytesSync(
+          encodePng(
+            copyResize(
+              image,
+              width: nu.toInt(),
+              interpolation: Interpolation.linear,
+            ),
+          ),
+        );
+        return nu;
+      }
+    case 15:
+      {
+        print('\nGenerating 1.5x image asset...');
+        double nu = (newWidth! * 6) / 8;
+        File('lib/assets/images/1.5x/$assetName.png').writeAsBytesSync(
+          encodePng(
+            copyResize(
+              image,
+              width: nu.toInt(),
+              interpolation: Interpolation.linear,
+            ),
+          ),
+        );
+        return nu;
+      }
+    case 1:
+      {
+        print('\nGenerating 1.0x image asset...');
+        double nu = (newWidth! * 4) / 6;
+        File('lib/assets/images/1.0x/$assetName.png').writeAsBytesSync(
+          encodePng(
+            copyResize(
+              image,
+              width: nu.toInt(),
+              interpolation: Interpolation.linear,
+            ),
+          ),
+        );
+        return nu;
+      }
+    case 75:
+      {
+        print('\nGenerating 0.75x image asset...');
+        double nu = (newWidth! * 3) / 4;
+        File('lib/assets/images/0.75x/$assetName.png').writeAsBytesSync(
+          encodePng(
+            copyResize(
+              image,
+              width: newWidth.toInt(),
+              interpolation: Interpolation.linear,
+            ),
+          ),
+        );
+        return nu;
+      }
+    default:
+      {
+        print('\nError generating image asset.');
+        exit(2);
+      }
+  }
 }
