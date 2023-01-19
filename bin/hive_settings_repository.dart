ca2f72fbe4a9b03963c609de9a9d6808cc8ba91d
Tulio Garcia Diniz <tuliao.gd@gmail.com>
2023-01-19 12:05:38 -0300

    :sparkles: feat: HiveSettingsRepository

---
+++ bin/hive_settings_repository.dart
@@ -0,0 +1,3 @@
+import 'package:fmagus/src/functions/hive_settings_repository_function.dart';
+
+void main() => hiveSettingsRepository();
---
+++ lib/src/functions/hive_settings_repository_function.dart
@@ -0,0 +1,93 @@
+import 'dart:io';
+
+/// Function to create Route Generator
+void hiveSettingsRepository() {
+  print(_start);
+
+  _createSettingsRepository();
+
+  print(_end);
+}
+
+//? PRIVATE
+//+ GETTERS
+String get _start => '''
+
+
+
+╔════════════════════════════════════════════════════════════════════════════╗
+║                                🧙 FMAGUS 🧙                                ║
+╠════════════════════════════════════════════════════════════════════════════╣
+║                 Starting Creating HiveSettingsRepository...                ║
+╚════════════════════════════════════════════════════════════════════════════╝
+''';
+
+String get _end => '''
+
+
+
+╔════════════════════════════════════════════════════════════════════════════╗
+║                                🧙 FMAGUS 🧙                                ║
+╠════════════════════════════════════════════════════════════════════════════╣
+║           HiveSettingsRepository Creating completed successfully!          ║
+╚════════════════════════════════════════════════════════════════════════════╝
+''';
+
+String get _settingsRepositoryInterface => '''
+abstract class ISettingsRepository {
+  //+ AppTheme
+  Future<String> getAppTheme();
+  Future<void> setAppTheme(
+    String value,
+  );
+}
+''';
+
+String get _settingsRepository => '''
+import 'package:hive/hive.dart';
+
+import 'interfaces/settings_repository_interface.dart';
+
+class SettingsRepository implements ISettingsRepository {
+  @override
+  Future<void> setAppTheme(
+    String value,
+  ) async {
+    Box box = await Hive.openBox(yourSettingsBoxName);
+
+    await box.put(yourThemeKey, value);
+
+    await box.close();
+  }
+
+  @override
+  Future<String> getAppTheme() async {
+    Box box = await Hive.openBox(yourSettingsBoxName);
+
+    String theme = box.get(
+      yourThemeKey,
+      defaultValue: yourLightThemeName,
+    );
+
+    await box.close();
+
+    return theme;
+  }
+}
+''';
+
+//+ FUNCTIONS
+void _createSettingsRepository() {
+  print('\n\nCreating ThemeConfigs...\n');
+  Directory('lib/repositories/interface').createSync(recursive: true);
+  File('lib/repositories/interfaces/settings_repository_interface.dart')
+      .writeAsStringSync(
+    _settingsRepositoryInterface,
+    mode: FileMode.writeOnly,
+  );
+  File('lib/repositories/settings_repository.dart').writeAsStringSync(
+    _settingsRepository,
+    mode: FileMode.writeOnly,
+  );
+  print('Creating Completed.');
+}
